840ebe923697dbd3494a5091e51db45f6273604f
YoshidaToshiki <toshiki1339@yahoo.co.jp>
2023-09-22 00:15:55 +0900

    add 記事投稿作成画面

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);  
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+        //return view('posts.show')->with(['posts' => $post->getPaginateByLimit(1)]);  
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+    }
+    public function create()
+    {
+    return view('posts.create');
+    }
+}
+?>
